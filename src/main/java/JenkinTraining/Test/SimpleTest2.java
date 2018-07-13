02c38790c5ca00ea2febafe421a114c16545bfe6
Gaurav <gaurav.jodh@perficient.com>
2018-07-13 18:25:54 +0530

    Second commit

--- src/main/java/JenkinTraining/Test/SimpleTest2.java
+++ src/main/java/JenkinTraining/Test/SimpleTest2.java
@@ -38,5 +38,60 @@ public class SimpleTest2 {
 	//print out the result
 	System.out.println("After Substraction Expected and Actual matched ");
 	 }
+	
+	@Test
+	public void divide(){
+	
+	int a =5, b = 6;
+	int c = b-a;
+	
+	Assert.assertEquals(c, 1);
+	//print out the result
+	System.out.println("After Substraction Expected and Actual matched ");
+	 }
+	
+	@Test
+	public void SquareNumber(){
+	
+	int a =5;
+	int b = a*a;
+	
+	Assert.assertEquals(b, 25);
+	//print out the result
+	System.out.println("After squaring the number Expected and Actual matched ");
+	 }
+	
+	@Test
+	public void squareNumberAdd(){
+	
+	int a =5, b = 6;
+	int c = (a*a) + (b*b);
+	
+	Assert.assertEquals(c, 60);
+	//print out the result
+	System.out.println("After adding square of the 2 number Expected and Actual matched ");
+	 }
+	
+	@Test
+	public void squareNumberSub(){
+	
+	int a =5, b = 6;
+	int c = (b*b) - (a*a);
+	
+	Assert.assertEquals(c, 11);
+	//print out the result
+	System.out.println("After substracting square of the 2 number Expected and Actual matched ");
+	 }
+	
+	@Test
+	public void CubeNumber(){
+	
+	int a = 5;
+	int b = 5*5*5;
+	
+	Assert.assertEquals(b, 125);
+	//print out the result
+	System.out.println("Cube: Expected and Actual matched ");
+	 }
 
 }
